51ec48d6ec236b0b4ee3eb45ff194ab9530927b7
Virendra kumar dwivedi <101566111+Virendrakumardwivedi@users.noreply.github.com>
2025-01-16 18:19:46 +0530

    navbar added

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import Link from "next/link";
+import styles from "./navbar.module.css";
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
-  )
-}
+    <nav className={styles.navbar}>
+      <div className={styles.logo}>
+        <img src="/nav.svg" alt="Logo" />
+      </div>
+      <ul className={styles.navLinks}>
+        <li>
+          <Link href="#">Hire Developers</Link>
+          <img src="/Vector.png" alt="Dropdown" className={styles.dropdownIcon} />
+        </li>
+        <li>
+          <Link href="#">Services</Link>
+          <img src="/Vector.png" alt="Dropdown" className={styles.dropdownIcon} />
+        </li>
+        <li>
+          <Link href="#">Technologies</Link>
+          <img src="/Vector.png" alt="Dropdown" className={styles.dropdownIcon} />
+        </li>
+        <li>
+          <Link href="#">Blogs</Link>
+          <img src="/Vector.png" alt="Dropdown" className={styles.dropdownIcon} />
+        </li>
+        <li>
+          <Link href="#">About Us</Link>
+        </li>
+      </ul>
+      <button className={styles.contactBtn}>Contact Us</button>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
