fc8e5d4e1a1f1485cfd03c0689c563f811daecbf
Sneha Vishwakarma <snehavish595@gmail.com>
2025-01-17 16:45:33 +0530

    Added new section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,29 @@
-import Navbar from "@/components/navbar/navbar";
+// import Navbar from "@/components/navbar/navbar";
+import ReadyToInnovate from "@/components/HomePage/ReadyToInnovate";
 import { Geist, Geist_Mono } from "next/font/google";
+import ITSolutions from "@/components/HomePage/ITSolutions";
+import SolutionsMatrix from "@/components/HomePage/SolutionsMatrix";
+import OurVision from "@/components/HomePage/OurVision";
+// import Whatwedo from "@/components/HomePage/Whatwedo";
+import Navbar from "@/components/navbar/navbar";
+import Footer from "@/components/footer/footer";
+import WhyChooseUs from "@/components/HomePage/whyChooseUs";
 
 
 export default function Home() {
   return (
-    <div>
-    <Navbar></Navbar>
+    <div className="container" style={{
+      backgroundColor: "#343333",
+      fontFamily:"gilroy"
+    }}>
+    <Navbar/>
+    <SolutionsMatrix/>
+    <ITSolutions/>
+    {/* <Whatwedo/> */}
+    <ReadyToInnovate/>
+    <OurVision/>
+    <WhyChooseUs/>
+    <Footer/>
     </div>
   );
 }
