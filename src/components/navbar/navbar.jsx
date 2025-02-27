3f207e7aa3f03551d5fc2e3a74cfb3b529fb8270
Shivi Chourey <choureyshivi02@gmail.com>
2025-02-27 15:37:00 +0530

    Created by Shivi

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
       ]
     },
     
-    "CMS & E-Commerce Developers": {
+    "CMS & E-Commerceeee Developers": {
       items: [
         { name: "Drupal Developer", iconSrc: "/Nav-Dropdown-icons/Drupal.svg", path: "drupal" },
         { name: "WordPress Developer", iconSrc: "/Nav-Dropdown-icons/WordPress.svg", path: "wordpress" },
