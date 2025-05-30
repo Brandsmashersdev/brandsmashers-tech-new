5ec744bb618fd0b35a9b7dacd418934bc65a2900
Brandsmashersdev <brandsmashersdev@gmail.com>
2025-05-30 18:00:28 +0530

    Added metadata

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+           <Head>
+      
+        <title>Brandsmashers Tech</title>
+
+        
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
       <body className="antialiased">
         <Main />
         <NextScript />
